de43909114e87b59381bf70259e2ffc801fbc509
Tahera29 <120545807+Tahera29@users.noreply.github.com>
2023-02-14 20:58:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your code here
 function mincosttoformrope(a) {
-	a.preventDefault();
+	a.preventDefault();//to stop vanishing data on refreshing page 
 var inputElement=document.querySelector('input').value;
 var arr=inputElement.split(',');// to convert into array
 	arr.sort(function(a,b) {return a-b});//a-b sort in increasing order b-a for decreeazing
@@ -13,7 +13,7 @@ var arr=inputElement.split(',');// to convert into array
 			arr.push(res);
 			cost+=res;
 			
-			arr.sort(function(a,b){return a-b});
+			arr.sort(function(a,b){return a-b});//data is being sort again and again so that i get the minimum element at front after push
 			
 		}
 	document.getElementById("result").textContent=cost;
