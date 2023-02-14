d41e18aefc1b09cbf983ca2c5509136290125db8
Tahera29 <120545807+Tahera29@users.noreply.github.com>
2023-02-14 20:54:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your code here
+function mincosttoformrope(a) {
+	a.preventDefault();
+var inputElement=document.querySelector('input').value;
+var arr=inputElement.split(',');// to convert into array
+	arr.sort(function(a,b) {return a-b});//a-b sort in increasing order b-a for decreeazing
+	var cost=0;
+	while(arr.length>1)
+		{
+			var res= Number(arr[0])+Number(arr[1]);
+			arr.shift();
+			arr.shift();//arr.splice(0,2);
+			arr.push(res);
+			cost+=res;
+			
+			arr.sort(function(a,b){return a-b});
+			
+		}
+	document.getElementById("result").textContent=cost;
+
+	
+}
